c0d1d6702ede7ed210c8a3d0a50fd3cffc94b054
Zerg Rush <34945957+zerg-rush@users.noreply.github.com>
2018-01-27 15:21:08 +0100

    eewvwrewv

--- src/main/java/Week5/App.java
+++ src/main/java/Week5/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int x = 5;
+        int y = 10;
+
+        System.out.println(Calculator.add(x, y));
+        System.out.println(Calculator.multiply(x, y));
     }
 }
