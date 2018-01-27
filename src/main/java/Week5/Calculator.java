70304e728fe6f88ba99a85bb804a344a6f5ecf65
Zerg Rush <34945957+zerg-rush@users.noreply.github.com>
2018-01-27 15:03:25 +0100

    eewvwrewv

---
+++ src/main/java/Week5/Calculator.java
@@ -0,0 +1,10 @@
+package Week5;
+
+public class Calculator {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
+}
